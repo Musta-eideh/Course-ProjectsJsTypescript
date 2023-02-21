e9f6bc19b216d65d081c13f5a85882225a847a19
Musta-eideh <mustafaeideh100@outlook.com>
2023-02-21 16:20:55 -0300

    Segunda forma exercício switch

---
+++ Switch2/assets/js/script.js
@@ -0,0 +1,28 @@
+const h1 = document.querySelector('.container h1');
+const data = new Date();
+
+function getDiaSemanaTexto(diaSemana) {
+    const diasSemana = ['domingo', 'segunda','terça','quarta','quinta','sexta', 'sábado'];
+    return diasSemana(diaSemana);
+}
+
+function getNomesMes(numeroMes) {
+    const diaMes = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'];
+    return meses [numeroMes];
+}
+
+function zeroAEsquerda(num) {
+    return num >= 10 ? num : `0${num}`;
+}
+
+function criaData(data) {
+    const diaSemana = data.getDay();
+    const numeroMes = data.getMonth();
+
+    const nomeDia = getDiaSemanaTexto(diaSemana);
+    const nomeMes = getNomesMes(numeroMes);
+
+    return `${nomeDia}, ${data.getDate()} de ${nomeMes} de ${data.getFullYear()} ${zeroAEsquerda(data.getHours())}:${zeroAEsquerda(data.getMinutes())}`
+}
+
+h1.innerHTML = criaData(data);
